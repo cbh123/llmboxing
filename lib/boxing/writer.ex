1f6a821a38ada83b3a158987bb2d49cb7be12e1b
Charlie Holtz <charlieholtz@gmail.com>
2023-07-24 11:44:36 -0400

    1000 questions

--- lib/boxing/writer.ex
+++ lib/boxing/writer.ex
@@ -15,7 +15,7 @@ defmodule Boxing.Writer do
   end
 
   def handle_info(:create, state) do
-    if Prompts.count_prompts() < 100 do
+    if Prompts.count_prompts() < 1000 do
       questions = Prompts.create_questions()
       questions |> Enum.each(fn q -> Prompts.generate(q) end)
       schedule_creation()
